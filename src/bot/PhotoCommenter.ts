74cc278ae76310a9420dbc1f099425a6a49af15a
Douglas Maitelli <dougmaitelli@gmail.com>
2020-02-07 15:11:43 -0800

    Update PhotoCommenter.ts

--- src/bot/PhotoCommenter.ts
+++ src/bot/PhotoCommenter.ts
@@ -2,6 +2,7 @@ import Bot from "./Bot";
 import TelegramBot = require("node-telegram-bot-api");
 
 const labelCommentMap = {
+  "corn|corn kernels|sweet corn": "MILHOOOOOO!!!!!",
   "cat|cats": "Aff, foto de gato denovo!",
   hamburger: "Meu deus! Um Hamburguer ia cair bem!!!",
   sushi: "Sushiiiiiii!!!",
@@ -10,7 +11,7 @@ const labelCommentMap = {
   "kayak|water":
     "Queria eu estar curtindo a água, mas isso iria fritar meus circuitos...",
   male: "Que homem...",
-  "female|beauty": "Olá gatinha...",
+  "female": "Olá gatinha...",
   "people|face": "Quem é esse feio?"
 };
 
